54467562123eb28c4b2da5e36cfd8618b7edc42f
anduril <egger@outlook.com>
2021-04-15 15:05:11 +0800

    step02 init
    step02 初始化

---
+++ learning-element2/step02/examples/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./logo.png" />
+    <JSX></JSX>
+    <h1>Welcome to My Vue.js UI By WebPack!</h1>
+  </div>
+</template>
+
+<script>
+import JSX from "./JSX.vue";
+export default {
+  name: "App",
+  components: { JSX },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
