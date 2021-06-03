06efab637ba7b3a1dcb79652028a04d61872b8a9
Anduril <76253631@qq.com>
2021-06-04 01:54:23 +0800

    中文注解
    中文注解

--- learning-element2/element-comment-zh_CN/build/md-loader/util.js
+++ learning-element2/element-comment-zh_CN/build/md-loader/util.js
@@ -1,14 +1,16 @@
-const { compileTemplate } = require('@vue/component-compiler-utils');
-const compiler = require('vue-template-compiler');
+const { compileTemplate } = require("@vue/component-compiler-utils");
+const compiler = require("vue-template-compiler");
 
+// 获取 <script> 标签中的文本内容
 function stripScript(content) {
   const result = content.match(/<(script)>([\s\S]+)<\/\1>/);
-  return result && result[2] ? result[2].trim() : '';
+  return result && result[2] ? result[2].trim() : "";
 }
 
+// 获取 <style> 标签中的文本内容
 function stripStyle(content) {
   const result = content.match(/<(style)\s*>([\s\S]+)<\/\1>/);
-  return result && result[2] ? result[2].trim() : '';
+  return result && result[2] ? result[2].trim() : "";
 }
 
 // 编写例子时不一定有 template。所以采取的方案是剔除其他的内容
@@ -17,27 +19,22 @@ function stripTemplate(content) {
   if (!content) {
     return content;
   }
-  return content.replace(/<(script|style)[\s\S]+<\/\1>/g, '').trim();
+  return content.replace(/<(script|style)[\s\S]+<\/\1>/g, "").trim();
 }
 
-function pad(source) {
-  return source
-    .split(/\r?\n/)
-    .map(line => `  ${line}`)
-    .join('\n');
-}
-
+// 参考 templateLoader.js  源码
+// 将自定义容器中的 代码块（fence） 转成一个个内联component注入到整个页面中
 function genInlineComponentText(template, script) {
   // https://github.com/vuejs/vue-loader/blob/423b8341ab368c2117931e909e2da9af74503635/lib/loaders/templateLoader.js#L46
   const finalOptions = {
     source: `<div>${template}</div>`,
-    filename: 'inline-component', // TODO：这里有待调整
-    compiler
+    filename: "inline-component",
+    compiler,
   };
   const compiled = compileTemplate(finalOptions);
   // tips
   if (compiled.tips && compiled.tips.length) {
-    compiled.tips.forEach(tip => {
+    compiled.tips.forEach((tip) => {
       console.warn(tip);
     });
   }
@@ -45,19 +42,20 @@ function genInlineComponentText(template, script) {
   if (compiled.errors && compiled.errors.length) {
     console.error(
       `\n  Error compiling template:\n${pad(compiled.source)}\n` +
-        compiled.errors.map(e => `  - ${e}`).join('\n') +
-        '\n'
+        compiled.errors.map((e) => `  - ${e}`).join("\n") +
+        "\n"
     );
   }
+  // 组件内容
   let demoComponentContent = `
     ${compiled.code}
   `;
-  // todo: 这里采用了硬编码有待改进
+  // script内容
   script = script.trim();
   if (script) {
-    script = script.replace(/export\s+default/, 'const democomponentExport =');
+    script = script.replace(/export\s+default/, "const democomponentExport =");
   } else {
-    script = 'const democomponentExport = {}';
+    script = "const democomponentExport = {}";
   }
   demoComponentContent = `(function() {
     ${demoComponentContent}
@@ -71,9 +69,17 @@ function genInlineComponentText(template, script) {
   return demoComponentContent;
 }
 
+// 每行增加空格
+function pad(source) {
+  return source
+    .split(/\r?\n/)
+    .map((line) => `  ${line}`)
+    .join("\n");
+}
+
 module.exports = {
   stripScript,
   stripStyle,
   stripTemplate,
-  genInlineComponentText
+  genInlineComponentText,
 };
